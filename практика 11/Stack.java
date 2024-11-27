8a4257b84ffac4b3864897bb6d5948ea2d9050fa
lymoos <livglass@yandex.ru>
2024-11-28 00:00:29 +0300

    fixes 11 word

--- практика 11/Stack.java
+++ практика 11/Stack.java
@@ -10,40 +10,30 @@ public class Stack {
         queue2 = new LinkedList<>();
     }
 
-    // Помещает элемент на вершину стека
     public void push(int x) {
         queue2.add(x);
         while (!queue1.isEmpty()) {
             queue2.add(queue1.poll());
         }
-        // Обмен очередей
-        Queue<Integer> temp = queue1;
+        Queue<Integer> temp = new LinkedList<>();
         queue1 = queue2;
         queue2 = temp;
     }
-
-    // Удаляет элемент с вершины стека и возвращает его
     public int pop() {
         if (queue1.isEmpty()) {
             throw new IllegalStateException("Stack is empty");
         }
         return queue1.poll();
     }
-
-    // Возвращает элемент на вершине стека
     public int top() {
         if (queue1.isEmpty()) {
             throw new IllegalStateException("Stack is empty");
         }
         return queue1.peek();
     }
-
-    // Проверяет, пуст ли стек
     public boolean empty() {
         return queue1.isEmpty();
     }
-
-    // Возвращает строковое представление стека
     @Override
     public String toString() {
         return queue1.toString();
--- практика 11/Test.java
+++ практика 11/Test.java
@@ -1,19 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        // Создание стека
         Stack stack = new Stack();
-
-        // Добавление элементов
         stack.push(10);
         stack.push(20);
-        stack.push(30);
-
         System.out.println("Элемент на верхушке: " + stack.top());
-
         stack.pop();
-
         System.out.println("Пустой? " + stack.empty());
-
         System.out.println("Элементы: " + stack.toString());
     }
 }
